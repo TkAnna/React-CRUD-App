6f5ea92bc8fd0eafb741156ab821cb306e4d59bd
Anna <tkachenko.an.an@gmail.com>
2023-10-23 11:30:14 +0200

    filters added

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -17,7 +17,8 @@ class App extends Component {
                 { name: 'Alex B.', salary: 3000, increase: true, rise: false, id: 2 },
                 { name: 'Bob N.', salary: 15000, increase: false, rise: false, id: 3 },
             ],
-            term: ''
+            term: '',
+            filter: 'all'
         }
         this.maxID = 4;
     }
@@ -101,11 +102,26 @@ class App extends Component {
         this.setState({ term }); // {term: term}
     }
 
+    filetrPost = (items, filter) => {
+        switch (filter) {
+            case 'rise':
+                return items.filter(item => item.rise);
+            case 'more1000':
+                return items.filter(item => item.salary > 1000);
+            default:
+                return items;
+        }
+    }
+
+    onFilterSelect = (filter) => {
+        this.setState({ filter });
+    }
+
     render() {
-        const { data, term } = this.state;
+        const { data, term, filter } = this.state;
         const emploees = this.state.data.length;
         const increased = this.state.data.filter(item => item.increase).length;
-        const visibleData = this.searchEmp(data, term);
+        const visibleData = this.filetrPost(this.searchEmp(data, term), filter);
 
         return (
             <div className='app'>
@@ -115,7 +131,7 @@ class App extends Component {
 
                 <div className="search-panel">
                     <SearchPanel onUpdateSearch={this.onUpdateSearch} />
-                    <AppFilter />
+                    <AppFilter filter={filter} onFilterSelect={this.onFilterSelect} />
                 </div>
 
                 <EmloyeesList
